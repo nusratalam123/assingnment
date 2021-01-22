e3f55011b08876575ec8e8535d84ba99ee3abfe9
nusratalam123 <nusratalam975@gmail.com>
2021-01-22 05:08:43 -0800

    sesh

--- mega.js
+++ mega.js
@@ -1,8 +1,12 @@
 function megaFriend(array){
+
     var maxArray=array[0]; // array ar first value ka define korse maxArray dara
+
         for(let j=0;j<array.length;j++){
+
             var elementArray=array[j];
             if(maxArray.length<elementArray.length){ //compeare two array value
+                
                 maxArray=elementArray;   //if elementArray gather than maxArray then this condition
 
             }
@@ -10,5 +14,6 @@ function megaFriend(array){
     return maxArray;
 }
 var array=["jagangiralam","mdsharafatullah","kalom","jahamana"];
+
 var largeName=megaFriend(array);
 console.log(largeName)
